7240cd861a029a7c6780314be2a6f3429bf52cea
Palak Kumari <148604703+Palak1725@users.noreply.github.com>
2024-11-17 20:57:30 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+'''youtuber = "Sahil Gaba"
+print("Subscribe to",youtuber)
+print("Subscribe to "+youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")'''
+
+ad = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer Programming is so {ad}! \
+It makes me so excited all the time because I love to {verb1}. \
+Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
